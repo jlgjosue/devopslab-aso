82e2cf2a71224d443113045069248cb3bebcdb05
Josue Luzardo Gebrim <josueluzardogebrim@gmail.com>
2021-11-17 21:57:29 -0300

    Bug Adicionado

--- app.py
+++ app.py
@@ -9,5 +9,14 @@ csrf = CSRFProtect(app)
 def pagina_inicial():
     return "Não pense que é capaz. Saiba que é. - Matrix"
 
+@app.route('/bug')                                                                                                                                
+def bad():                                                                                                                                        
+    try:                                                                                                                                          
+        raise TypeError()                                                                                                                         
+    except TypeError as e:                                                                                                                        
+        print(e)                                                                                                                                  
+    except TypeError as e:                                                                                                                        
+        print("Duplicado, ou seja, nunca vai entrar aqui.")      
+
 if __name__ == '__main__':
     app.run()
