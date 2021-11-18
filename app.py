87037b87963201b2e042b989f77b05b4f760eb7f
Josue Luzardo Gebrim <josueluzardogebrim@gmail.com>
2021-11-17 22:01:58 -0300

    Bug Removido

--- app.py
+++ app.py
@@ -8,15 +8,7 @@ csrf = CSRFProtect(app)
 @app.route("/")
 def pagina_inicial():
     return "Não pense que é capaz. Saiba que é. - Matrix"
-
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")      
+   
 
 if __name__ == '__main__':
     app.run()
